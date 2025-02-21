81c303d34be4a230f04c8120e3732e3c23309e01
mopspear <123269266+nikuwebdeveloper@users.noreply.github.com>
2025-02-21 15:24:43 -0500

    Various Small Updates
    - updated logo
    - improved function for small screens
    - improved general responsiveness
    - fixed text errors with light box on art page
    - fixed footer placement
    - added hover secret for meat image in the footer

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttonTop = document.getElementById('button-top');
-    const moptecAnchor = document.getElementById('moptec');
-    const moptecHeading2 = document.getElementById('moptec-under-construction');
+// document.addEventListener("DOMContentLoaded", function () {
+//     const buttonTop = document.getElementById('button-top');
+//     // const moptecAnchor = document.getElementById('moptec');
+//     const moptecHeading2 = document.getElementById('moptec-under-construction');
 
 
-    function getWindowHeight() {
-        return document.documentElement.scrollHeight - window.innerHeight;
-    }
+//     function getWindowHeight() {
+//         return document.documentElement.scrollHeight - window.innerHeight;
+//     }
 
-    function buttonTopOpacity() {
-        let windowHeight = getWindowHeight();
-        let scrollPercentage = window.scrollY / windowHeight;
-        buttonTop.style.opacity = Math.min(scrollPercentage, 1);
-    }
+//     // function buttonTopOpacity() {
+//     //     let windowHeight = getWindowHeight();
+//     //     let scrollPercentage = window.scrollY / windowHeight;
+//     //     buttonTop.style.opacity = Math.min(scrollPercentage, 1);
+//     // }
 
-    function displayUnderConstruction() {
-        moptecHeading2.innerHTML = "COMING SOON";
+//     function displayUnderConstruction() {
+//         moptecHeading2.innerHTML = "COMING SOON";
 
-    }
-    function resetText() {
-        moptecHeading2.innerHTML = "MOPTEC";
-    }
+//     }
+//     function resetText() {
+//         moptecHeading2.innerHTML = "MOPTEC";
+//     }
 
-    moptecAnchor.addEventListener('mouseover', displayUnderConstruction);
-    moptecAnchor.addEventListener('mouseout', resetText);
-    window.addEventListener('scroll', buttonTopOpacity);
+//     // moptecAnchor.addEventListener('mouseover', displayUnderConstruction);
+//     // moptecAnchor.addEventListener('mouseout', resetText);
+//     // window.addEventListener('scroll', buttonTopOpacity);
 
-});
+// });
